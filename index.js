ad84628f1f0d2a9514ec62cdd39691ae192a85c3
akbar19910918 <akbarachilov91@gmail.com>
2023-03-07 13:28:18 +0500

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+//            karra jadvali
+
+//  for(i=2;i<9;i++){
+//     console.log(" ");
+//     console.log(`${i} karra jadvali`);
+//     console.log('');
+//     for(j=1;j<10;j++){
+//       console.log(`${i} * ${j} = ${i*j} `);  
+//     }
+// }
+
+
+//      1 dan 100gacha bolgan sonlar ichidan 3ga ham 5ga ham bolinadiganlarini chiqarish
+
+// for(i=1;i<=100;i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(i);
+//     } 
+// }
+
+
+
+// for(i=1;1<5;i++){
+//     for(j=1;j<3;j++){
+//     console.log(j + i);
+// }
+//      console.log("=====");
+// }
+// let res=1;
+// while(res<=10 && res>-10){
+//     console.log(res);
+//     res--;
+  
+// }
+//console.log(new Date().getDay());
+// for(i=1;i<=100;i++){
+//     for(j=2;j<=i;j++){
+        
+//     }
+//}
