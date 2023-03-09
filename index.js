870940aa73aa476ef6d0bb4891ba07067aad9475
akbar19910918 <akbarachilov91@gmail.com>
2023-03-09 15:26:37 +0500

    masala

---
+++ index.js
@@ -0,0 +1,66 @@
+//           object masalalar
+
+//                   1-masala
+
+// let user={
+//     name:"John",
+//     surname:"Smith",
+
+// }
+
+// console.log(user);
+
+// let user={
+//     name:"John",
+//     surname:"Smith",
+//     name:"Peta"
+// }
+
+// console.log(user);
+
+// let user={
+//     name:"John",
+//     surname:"Smith",
+//     name:"Peta"
+// }
+// delete user.name
+// console.log(user);
+
+//                          2-masala
+
+// salaries = sal
+
+// let sal = {
+//    John: 100,
+//    Ann: 160,
+//    Pete: 130
+// }
+// for (i in sal) 
+//       sal[i] = sal[i] * 2
+  
+// console.log(Object.values(sal));
+
+ //                           3-masala
+
+// // before the call
+
+//  let menu = {
+//     width:200,
+//     height:300,
+//     title:"My menu"
+//  };
+// for(i in menu){
+//    if(typeof menu[i] =="number"){
+//      menu[i] = menu[i] * 2
+//    }
+// }
+// console.log(menu);
+
+//  multiplyNumeric(menu);
+
+//  // after the call
+//  menu = {
+//     width:400,
+//     height:600,
+//     title:"My menu"
+//  };
